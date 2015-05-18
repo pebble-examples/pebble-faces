a2e271824af7a80b41e789937d308f5a3b8d82c0
Varun Reddy <vreddy@getpebble.com>
2015-05-18 11:01:17 -0700

    Added conversion for OG Pebble

--- URLtoPebblePng.py
+++ URLtoPebblePng.py
@@ -5,7 +5,7 @@ import argparse
 import os
 
 #Get image from URL and generate pebble compliant png
-def get_pebble_png(input_url):
+def get_pebble_png(input_url, PebbleType):
     img = urllib2.urlopen(input_url)
     localFile = open('TempImage.jpg', 'wb')
     localFile.write(img.read())
@@ -25,10 +25,15 @@ def get_pebble_png(input_url):
     #resizing image to fit Pebble screen
     im_smaller = tempIm.resize(size,Image.ANTIALIAS)
 
-    #converting to 64 color Pebble scheme and dithering image
-    dithered_im = im_smaller.convert(mode='P',
-     colors=64,
-     palette=Image.FLOYDSTEINBERG)
+    if PebbleType == 1:
+        #converting to 64 color Pebble scheme and dithering image
+        dithered_im = im_smaller.convert(mode='P',
+          colors=64,
+          palette=Image.FLOYDSTEINBERG)
+    else:
+        #converting to graysclae and then dithering to black and white
+        im_tempo = im_smaller.convert('LA')
+        dithered_im = im_smaller.convert('1')
 
     #saving dithered image as PNG
     dithered_im.save("Pebble_image.png","PNG")
@@ -40,8 +45,9 @@ def main():
     parser = argparse.ArgumentParser(
         description='Get image from URL and convert to 64-color palettized PNG for Pebble Time')
     parser.add_argument('input_url', type=str, help='URL from which to convert image')
+    parser.add_argument('PebbleType', type=int, help='0 is OG Pebble, 1 is Pebble Time')
     args = parser.parse_args()
-    get_pebble_png(args.input_url)
+    get_pebble_png(args.input_url, args.PebbleType)
 
 if __name__ == '__main__':
     main()
